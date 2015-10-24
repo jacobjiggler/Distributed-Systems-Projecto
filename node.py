9780b70ee9d2e0c342ab2dfa5b01682061d9b2cd
Mikey <micmoo@me.com>
2015-10-24 14:08:38 -0400

    premerge

--- node.py
+++ node.py
@@ -1,6 +1,8 @@
 import SocketServer
 from threading import Thread
 from sys import argv
+from calendar import *
+from time_table import *
 import socket
 node = None
 
@@ -8,14 +10,15 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-
-        self.request.sendall("ACK") #replace with some appropriate responce...
         if node:
             node.receive(self.data)
 
 class Node():
-    def __init__(self, ip):
-        self.ip = ip
+    ips = []
+    def __init__(self, _id):
+        self.id = _id
+        self.ip = Node.ips[self.id]
+        
         listener = SocketServer.TCPServer((self.ip, 6000), MyTCPHandler)
         self.thread = Thread(target = listener.serve_forever)
         self.thread.start()
@@ -25,23 +28,26 @@ class Node():
         print "Received Data"
         print data
 
-    def send(self, ip):
+    def send(self, _id):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             data = "data"
             print "Sending Data from client"
             # Connect to server and send data
-            sock.connect((ip, 6000))
+            sock.connect((Node.ips[_id], 6000))
             sock.sendall(data + "\n")
 
             # Receive data from the server and shut down
             received = sock.recv(1024)
-            print "ACK: " + received
+            # Add To EntrySet
+        except : 
+            pass
+            # Node Down cancel conflict
         finally:
             sock.close()
 
 
 if __name__ == "__main__":
+    Node.ips = open('ip', 'r').read().split("\n")[0:4]
     node = Node(argv[1])
-    dic = EntrySet()
 
