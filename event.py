1b9df3ef7964f8be631b0df5319086d761dfbed3
Mikey <micmoo@me.com>
2015-10-25 21:17:26 -0400

    patchjson

--- event.py
+++ event.py
@@ -11,6 +11,8 @@ class Event:
         self.entry = entry
     
     def dicttester(self, o):
+        if hasattr(o, 'thread'):
+            return o.id
         if hasattr(o, '__dict__'):
             return o.__dict__
         else:
