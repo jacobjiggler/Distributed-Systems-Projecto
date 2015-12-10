d9f8dd5cc3d1da9b45d6fbfc30f3e126e24148b4
Mikey <micmoo@me.com>
2015-12-09 21:43:45 -0500

    aaa

--- node.py
+++ node.py
@@ -90,6 +90,10 @@ class Node():
 
     def send(self, event=None):
         _id = Node.ips[self.agent.leader]
+        data = {'event':event.to_JSON(), 'hash' : self.entry_set.hash, 'type' : 'event'}
+        if (self.agent.leader == self.id):
+            self.agent.receive(data)
+            return
         
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
         data = {'event':event.to_JSON(), 'hash' : self.entry_set.hash, 'type' : 'event'}
